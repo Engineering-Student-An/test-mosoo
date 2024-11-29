39aa512129a8accba58eacedbb72074f38a53340
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-11-29 22:35:37 +0900

    Initial commit

---
+++ src/test/java/study/testmosoo/TestMosooApplicationTests.java
@@ -0,0 +1,13 @@
+package study.testmosoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMosooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
